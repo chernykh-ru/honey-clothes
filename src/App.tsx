f5c2b6ffedf8120b58f303ceb266131096a38c6e
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-05-13 12:56:04 +0300

    added basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './routes/Home/Home'
+import Navigation from './routes/Navigation/Navigation'
 
-function App() {
-  return <div>Honey Clothes</div>
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
 export default App
