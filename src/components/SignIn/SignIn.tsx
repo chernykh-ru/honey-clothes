a61b321033bc32c9a9cd6aa86394d510f479091c
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-05-16 13:33:01 +0300

    added sign in with google redirect auth in firebase

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getRedirectResult } from 'firebase/auth'
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
+  useEffect(() => {
+    const fetchResult = async () => {
+      try {
+        const response = await getRedirectResult(auth)
+        if (response) {
+          const userSnapshot = await createUserDocumentFromAuth(response.user)
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log('not auth with redirect', error.message)
+        }
+      }
+    }
+    fetchResult()
+  }, [])
+
   const signInWithGoogle = async () => {
     const { user } = await signInWithGooglePopup()
     const userSnapshot = await createUserDocumentFromAuth(user)
   }
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={signInWithGoogle}>Sign In with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign In with Google Redirect
+      </button>
     </div>
   )
 }
