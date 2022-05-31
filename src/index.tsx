a1b6781c538721d480f12e27d80739cf2ce8a2cd
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-05-31 14:02:41 +0300

    added using redux persist for cart slice (saving orders when the user reloads the page)

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,20 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { setupStore } from './store/store'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const store = setupStore()
+const persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,17 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import CartReducer from '../store/reducers/cartSlice'
 import UserReducer from '../store/reducers/userSlice'
 import CategoryReducer from '../store/reducers/categorySlice'
+import {
+  persistReducer,
+  PersistConfig,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 const rootReducer = combineReducers({
   cart: CartReducer,
@@ -9,13 +20,33 @@ const rootReducer = combineReducers({
   category: CategoryReducer,
 })
 
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[]
+}
+
+const persistConfig: ExtendedPersistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const setupStore = () => {
   return configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
         serializableCheck: {
-          ignoredActions: ['user/setCurrentUser'],
+          ignoredActions: [
+            'user/setCurrentUser',
+            FLUSH,
+            REHYDRATE,
+            PAUSE,
+            PERSIST,
+            PURGE,
+            REGISTER,
+          ],
           ignoredPaths: ['user.currentUser'],
         },
       }),
